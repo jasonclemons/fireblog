3a5acb041e2abca8259990a0badcfa891095fd59
Jason Clemons <jason@clemons.io>
2020-06-08 22:28:00 -0400

    Update index.php
    ! Added template functions
    ! Added more documentation

--- index.php
+++ index.php
@@ -11,8 +11,11 @@ init();
 
 /**
  * Init
+ *
+ * @return void
  */
 function init() {
+
 	// Load our language strings
 	load_language();
 
@@ -58,6 +61,36 @@ function init() {
 			header('Location: config');
 		}
 	}
+
+	// Set up our valid endpoints
+	$endpoints = array(
+		'post',
+		'destroy',
+		'edit',
+		'setup',
+		'update'
+	);
+
+	// Figure out the page title
+	if (count($uri_parts) >= 1 && $uri_parts[0]) {
+		$page_title = ucwords($uri_parts[0]);
+	} else if (count($uri_parts) = 2) {
+		$page_title = null; //$post_data['title'];
+	} else {
+		$page_title = 'Index'; //langstring
+	}
+
+	// Figure out what template to show
+	if (count($uri_parts) >= 1 && $uri_parts[0]) {
+		if (in_array($uri_parts[0], $endpoints)) {
+			$template = '_page_' . $uri_parts[0];
+			_page_template_above();
+			$template();
+			_page_template_below();
+		} else {
+			header('Location: /');
+		}
+	}
 }
 
 function _page_config() {
@@ -76,10 +109,62 @@ function _page_destroy() {
 
 }
 
+function _page_edit() {
+
+}
+
+function _page_template_above() {
+	echo '<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>' . $config['blog_name'] . ' | ' . $page_title . '</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.5/examples/blog/blog.css" rel="stylesheet">
+  </head>
+  <body>';
+}
+
+function _page_template_below() {
+	echo '<footer class="blog-footer">
+  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+</body>
+</html>';
+}
+
 /**
  * Create config file
  *
  * @param array $config_options
+ * @return void
  */
 function create_config($config_options) {
 	// Parse out the options into text
@@ -99,6 +184,7 @@ function create_config($config_options) {
  * Sanitize our user input
  *
  * @param string $input
+ * @return string
  */
 function sanitize_input($input) {
 	$input = trim($input);
@@ -112,6 +198,7 @@ function sanitize_input($input) {
  * Create URL slug
  *
  * @param string $slug
+ * @return string
  */
 function create_slug($slug) {
 	$slug = strtolower($slug);
@@ -122,6 +209,8 @@ function create_slug($slug) {
 
 /**
  * Load up all language strings
+ *
+ * @return void
  */
 function load_language() {
 	$language['errors']['permissions']['posts'] = 'Unable to create /posts directory. Make sure your root directory has the correct permissions (755).';
